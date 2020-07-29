9199db476182b238e3d7f89851ba108b2ee11218
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2020-07-30 00:30:34 +0530

    first commit

---
+++ maven-git-demo/src/main/java/com/manipal/maven_git_demo/App.java
@@ -0,0 +1,14 @@
+package com.manipal.maven_git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // https://github.com/kumareshgupta/maven-git-repo.git
+    }
+}
